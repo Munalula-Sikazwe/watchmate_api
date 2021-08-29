75ea57f240ab3183871e734f32e10eabdbb7f1ec
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-08-29 20:15:07 +0200

    created watchlist api view.

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,7 +1,7 @@
 from  django.urls import path
-from .views import GetCreateStreamPlatformAV
+from .views import GetCreateStreamPlatformAV,GetCreateWatchListAV
 app_name = 'watchlist'
 urlpatterns = [
-    path('stream-platform',GetWatchListAV.as_view(),),
+    path('stream-platform',GetCreateWatchListAV.as_view(),),
     path('watchlist',GetCreateStreamPlatformAV.as_view())
 ]
