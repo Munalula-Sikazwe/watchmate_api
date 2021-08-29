4364a871170a1982a32ee5212cb5ed933ead6830
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-08-29 18:26:30 +0200

    completed watchlist model design.

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class WatchList(models.Model):
-    name = models.CharField(max_length=50)
-    description = models.TextField(max_length=100)
+    title = models.CharField(max_length=50)
+    story_line = models.TextField(max_length=100)
     active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
