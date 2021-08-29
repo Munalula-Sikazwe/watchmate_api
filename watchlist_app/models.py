be2d0ac8c114b4f0ec36152f315cb80d15c400f7
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-08-29 18:30:08 +0200

    migrated watchlist model.

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -8,4 +8,12 @@ class WatchList(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        return self.title
+
+class StreamPlatform(models.Model):
+    name = models.CharField(max_length=100)
+    about = models.TextField(max_length=100)
+    website = models.URLField(max_length=100)
+
+    def __str__(self):
         return self.name
