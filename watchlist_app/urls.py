11e7d029c8d9e8cd0d893255eb69beeed71afbaa
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-07 20:31:50 +0200

    created single watchlist endpoint.

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,9 +1,10 @@
 from  django.urls import path
-from .views import GetCreateStreamPlatformAV, GetCreateWatchListAV, GetSingleStreamPlatformAv
+from .views import GetCreateStreamPlatformAV, GetCreateWatchListAV, GetSingleStreamPlatformAv, GetSingleWatchListAV
 
 app_name = 'watchlist'
 urlpatterns = [
-    path('stream-platform',GetCreateWatchListAV.as_view(),),
-    path('stream-platform/<int:pk>',GetSingleStreamPlatformAv.as_view(),),
-    path('watchlist',GetCreateStreamPlatformAV.as_view())
+    path('stream-platform',GetCreateWatchListAV.as_view()),
+    path('stream-platform/<int:pk>',GetSingleStreamPlatformAv.as_view()),
+    path('watchlist',GetCreateStreamPlatformAV.as_view()),
+    path('watchlist/<int:pk>',GetSingleWatchListAV.as_view()),
 ]
