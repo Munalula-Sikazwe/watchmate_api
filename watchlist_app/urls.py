71814329609a28c938e20793f45ef806f241165d
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-07 20:29:53 +0200

    created path for single stream-platform.

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -1,7 +1,9 @@
 from  django.urls import path
-from .views import GetCreateStreamPlatformAV,GetCreateWatchListAV
+from .views import GetCreateStreamPlatformAV, GetCreateWatchListAV, GetSingleStreamPlatformAv
+
 app_name = 'watchlist'
 urlpatterns = [
     path('stream-platform',GetCreateWatchListAV.as_view(),),
+    path('stream-platform/<int:pk>',GetSingleStreamPlatformAv.as_view(),),
     path('watchlist',GetCreateStreamPlatformAV.as_view())
 ]
