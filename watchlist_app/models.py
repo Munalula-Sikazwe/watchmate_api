0fbda88105f613144def5fccfeaf3de5eb6cfaa2
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-07 20:55:31 +0200

    created model relationships between watchlist and streamplatform

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,14 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class WatchList(models.Model):
-    title = models.CharField(max_length=50)
-    story_line = models.TextField(max_length=100)
-    active = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
+from django.db.models import CASCADE
 
-    def __str__(self):
-        return self.title
 
 class StreamPlatform(models.Model):
     name = models.CharField(max_length=100)
@@ -17,3 +11,15 @@ class StreamPlatform(models.Model):
 
     def __str__(self):
         return self.name
+
+class WatchList(models.Model):
+    title = models.CharField(max_length=50)
+    story_line = models.TextField(max_length=100)
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    platform = models.ForeignKey(StreamPlatform,on_delete=CASCADE,related_name='watchlist')
+
+    def __str__(self):
+        return self.title
+
+
