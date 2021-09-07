4540e1c3c72a61ea08c3f01ea10a9416b15a0b11
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-07 20:26:10 +0200

    performed some minor formatting.

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from rest_framework.generics import  ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
 # Create your views here.
 from watchlist_app.models import StreamPlatform, WatchList
 from watchlist_app.serializers import StreamPlatformSerializer, WatchListSerializer
@@ -8,11 +8,13 @@ from watchlist_app.serializers import StreamPlatformSerializer, WatchListSeriali
 class GetCreateStreamPlatformAV(ListCreateAPIView):
     queryset = StreamPlatform.objects.all()
     serializer_class = StreamPlatformSerializer
+
+
 class GetSingleStreamPlatformAv(RetrieveUpdateDestroyAPIView):
     queryset = StreamPlatform.objects.all()
     serializer_class = StreamPlatformSerializer
-    
+
+
 class GetCreateWatchListAV(ListCreateAPIView):
     queryset = WatchList.objects.all()
     serializer_class = WatchListSerializer
-
