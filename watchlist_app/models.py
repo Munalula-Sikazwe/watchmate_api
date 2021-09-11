229645d572b66091a85d2452598858e3d0a7dbc8
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-11 12:31:40 +0200

    performed some minor housekeeping to the models file.

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -13,21 +13,22 @@ class StreamPlatform(models.Model):
     def __str__(self):
         return self.name
 
+
 class WatchList(models.Model):
     title = models.CharField(max_length=50)
     story_line = models.TextField(max_length=100)
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    platform = models.ForeignKey(StreamPlatform,on_delete=CASCADE,related_name='watchlist',null=True)
+    platform = models.ForeignKey(StreamPlatform, on_delete=CASCADE, related_name='watchlist', null=True)
 
     def __str__(self):
         return self.title
 
 
 class Reviews(models.Model):
-    rating = models.PositiveIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
-    description = models.TextField(max_length=200,null=True)
-    watchlist = models.ForeignKey(WatchList,on_delete=models.CASCADE,related_name='reviews')
+    rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    description = models.TextField(max_length=200, null=True)
+    watchlist = models.ForeignKey(WatchList, on_delete=models.CASCADE, related_name='reviews')
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
