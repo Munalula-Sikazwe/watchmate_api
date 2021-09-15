8f9b1d25243a983d9fcd5f2ff2d9b9680471fa7e
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-15 22:33:53 +0200

    allowed reviewer to only create a review when it does not exist.

--- watchlist_app/views.py
+++ watchlist_app/views.py
@@ -1,8 +1,9 @@
-
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView,GenericAPIView
+import reviewer as reviewer
+from requests import Response
+from rest_framework import status
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 # Create your views here.
-from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from watchlist_app.models import StreamPlatform, WatchList, Review
 from watchlist_app.serializers import StreamPlatformSerializer, WatchListSerializer, ReviewSerializer
@@ -40,9 +41,15 @@ class GetCreateReview(ListCreateAPIView):
     def get_queryset(self):
         return self.queryset.filter(watchlist=self.kwargs.get('pk'))
 
+    def perform_create(self, serializer):
+        reviewer = self.request.user
+        queryset = self.queryset.filter(watchlist=self.kwargs.get('pk'), reviewer=reviewer)
+        if queryset.exists():
+            return Response({"error": "You have already made a review for this watchlist."},
+                            status=status.HTTP_409_CONFLICT)
+        serializer.save(reviewer=reviewer)
+
+
 class StreamPlatformVs(ModelViewSet):
     queryset = StreamPlatform.objects.all()
     serializer_class = StreamPlatformSerializer
-
-
-
