cbb84afe6dd73d3b6b557cb4ae0071c42e0afa16
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-18 19:37:34 +0200

    changed to custom user admin in the dashboard.

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import WatchList, StreamPlatform, Review, MyUser
 
 # Register your models here.
-
+class CustomUserAdmin(UserAdmin):
+    pass
 admin.site.register(WatchList)
 admin.site.register(StreamPlatform)
 admin.site.register(Review)
