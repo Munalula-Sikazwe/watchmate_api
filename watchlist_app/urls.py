f04fc324d7eb5a5dbc8f8c3072df65fee837520d
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-18 15:53:33 +0200

    made changes to reviews urls.

--- watchlist_app/urls.py
+++ watchlist_app/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('stream-platform/<int:pk>',GetSingleStreamPlatformAv.as_view(),name='streamplatform-detail'),
     path('stream-platform',GetCreateStreamPlatformAV.as_view()),
     path('watchlist/<int:pk>',GetSingleWatchListAV.as_view(),name='watchlist-detail'),
-    path('watchlist/<int:pk>/reviews',GetCreateReview.as_view(),name='watchlist-reviews'),
-    path('watchlist/reviews/<int:pk>',GetSingleReview.as_view(),name='watchlist-reviews-detail'),
+    path('<int:pk>/reviews',GetCreateReview.as_view(),name='watchlist-reviews'),
+    path('reviews/<int:pk>',GetSingleReview.as_view(),name='watchlist-reviews-detail'),
     path('streamplatform',include(router.urls))
 ]
