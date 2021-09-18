f5818acfc29e2093fc6bb9337f3663f2e0e1a4b2
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-18 14:24:36 +0200

    changed the serializer depth for testing.

--- watchlist_app/serializers.py
+++ watchlist_app/serializers.py
@@ -15,7 +15,7 @@ class ReviewSerializer(ModelSerializer):
 
 class WatchListSerializer(ModelSerializer):
     reviews = StringRelatedField(many=True)
-
+    depth = 2
 
 
     class Meta:
@@ -25,7 +25,7 @@ class WatchListSerializer(ModelSerializer):
 
 class StreamPlatformSerializer(ModelSerializer):
     watchlist = WatchListSerializer(read_only=True, many=True)
-
+    depth = 2
     class Meta:
         model = StreamPlatform
         fields = '__all__'
