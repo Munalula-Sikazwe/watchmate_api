82f9de8433282fe2abc51a45f15fed38ff2167b5
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-18 15:08:06 +0200

    check for reviewer permissions.

--- watchlist_app/permissions.py
+++ watchlist_app/permissions.py
@@ -1,6 +1,16 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
+
 class UserPermissions(BasePermission):
-## check if the user is a director.
-    def has_permission(self,request,view):
+    ## check if the user is a director.
+    def has_permission(self, request, view):
         return request.user.user_role == 'customer'
+
+
+class ReviewPermissions(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method == permissions.SAFE_METHODS:
+            return True
+        else:
+            return obj.reviewer == request.user
