8062526137516ff20f4fea57b4d7ea35e58caef9
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-24 14:46:48 +0200

    created logout view.

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
-from user_app.views import RegistrationView
+from user_app.views import RegistrationView, LogoutView
 
 app_name = 'user'
 urlpatterns = [
     path('login',obtain_auth_token,name='login'),
     path('registration',RegistrationView.as_view(),name='registration'),
+    path('logout',LogoutView.as_view(),name='logout')
 ]
