5ffdc139dbdc5473c799eec370707080a1ee2e10
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-24 14:45:01 +0200

    minor optimizations.

--- user_app/views.py
+++ user_app/views.py
@@ -1,12 +1,8 @@
-
-
 # Create your views here.
 from django.contrib.auth import get_user_model
-from rest_framework.views import APIView
-
-from . import  models
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 
 from user_app.serializers import RegistrationSerializer
 
@@ -17,9 +13,9 @@ class RegistrationView(CreateAPIView):
     serializer_class = RegistrationSerializer
     permission_classes = [AllowAny]
 
+
 class LogoutView(APIView):
 
-    def post(self,request):
+    def post(self, request):
         if request.user.is_authenticated():
             request.user.authtoken.delete()
-
