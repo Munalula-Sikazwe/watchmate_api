932fefb9025a06342f5f0375a393f0b11b7a2a78
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-24 10:25:24 +0200

    created registration view first instance.

--- user_app/urls.py
+++ user_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 app_name = 'user'
 urlpatterns = [
-    path('login',obtain_auth_token,name='login')
+    path('login',obtain_auth_token,name='login'),
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+
 
 # Create your views here.
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+
+from user_app.serializers import RegistrationSerializer
+
+
+class RegistrationView(CreateAPIView):
+    user = get_user_model()
+    queryset = user.objects.all()
+    serializer_class = RegistrationSerializer
+
+    
+
