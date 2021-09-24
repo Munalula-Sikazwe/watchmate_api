39658fed38efbf4ce79b5cc72142d4b701836c29
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-24 15:33:18 +0200

    created logout view.

--- user_app/views.py
+++ user_app/views.py
@@ -1,7 +1,9 @@
 # Create your views here.
 from django.contrib.auth import get_user_model
+from rest_framework import status
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from user_app.serializers import RegistrationSerializer
@@ -19,3 +21,4 @@ class LogoutView(APIView):
     def post(self, request):
         if request.user.is_authenticated():
             request.user.authtoken.delete()
+            return Response({"success":"You have successfully logged out."},status=status.HTTP_200_OK)
