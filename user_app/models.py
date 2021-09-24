c283bb018d1db563b776ad53313070eccab25cbe
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-09-24 14:31:57 +0200

    added signals to create user token on startup.

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver, Signal
+from rest_framework.authtoken.models import Token
+
+users = get_user_model()
+
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+
+post_save.connect(create_auth_token,sender=settings.AUTH_USER_MODEL)
