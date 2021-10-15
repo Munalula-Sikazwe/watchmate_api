3cb889bcf342901d9f5e1dba3bb37efd9567481a
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-10-15 15:10:17 +0200

    added django filters to installed apps.

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
 from user_app.views import RegistrationView, LogoutView
 
@@ -7,5 +8,7 @@ app_name = 'user'
 urlpatterns = [
     path('login',obtain_auth_token,name='login'),
     path('registration',RegistrationView.as_view(),name='registration'),
-    path('logout',LogoutView.as_view(),name='logout')
+    path('logout',LogoutView.as_view(),name='logout'),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- watchmate/settings.py
+++ watchmate/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'watchlist_app.apps.WatchlistAppConfig',
     'rest_framework',
     'rest_framework.authtoken',
+    'django_filters'
 
 ]
 
